bae068b1c676a063b11583b08e5d1c83047cb45e
Michal Nowak <miau.nowak@gmail.com>
2020-02-07 12:42:42 +0100

    class style js

---
+++ app/js/Slider.js
@@ -0,0 +1,92 @@
+export default class Slider {
+    constructor() {
+        this.imagesContainerEl = document.querySelector('.slider__images-container');
+        this.imgEl1 = document.querySelector('.slider__image-container--first img');
+        this.imgEl2 = document.querySelector('.slider__image-container--second img');
+
+        this.dragging = false;
+        this.img2ContainerEl = document.querySelector('.slider__image-container--second');
+        this.handleEl = document.querySelector('.slider__handle');
+        this.dividerEl = document.querySelector('.slider__divider');
+        this.imagesContainerWidth;
+        this.imagesContainerLeftOffset;
+        this.init();
+    }
+
+
+
+
+    // metody
+
+    // clientX to przesuniecie w lini poziomej w px - potrzebna do slidera
+    //clientX
+    getOffset = (clientX) => {
+        const offset = clientX - this.imagesContainerLeftOffset;
+        if (offset < 0) {
+            return 0;
+        } else if (offset > this.imagesContainerWidth) {
+            return this.imagesContainerWidth
+        } else return offset;
+    };
+
+    move = (clientX) => {
+        const offset = this.getOffset(clientX);
+        // zmierzamy do wartosci procentowej
+        const percent = offset / this.imagesContainerWidth * 100;
+        // console.log(percent);
+
+        this.dividerEl.style.left = `${percent}%`;
+        //zmieniamy drugiemu obrazkowi  wielkosc
+        this.img2ContainerEl.style.width = `${percent}%`;
+    }
+
+
+    // funkcja wywolujaca
+    events = () => {
+        this.handleEl.addEventListener('mousedown', () => {
+            this.dragging = true
+        });
+        this.handleEl.addEventListener('mouseup', () => {
+            this.dragging = false
+        });
+        //urzadzenia mobilne 
+        this.handleEl.addEventListener('touchstart', () => {
+            this.dragging = true
+        });
+        this.handleEl.addEventListener('touchend', () => {
+            this.dragging = false
+        });
+
+        window.addEventListener('touchmove', (event) => {
+            if (this.dragging) {
+                this.move(event.touches[0].clientX);
+            } else
+                return
+        });
+
+        //przekazujemy event
+        window.addEventListener('mousemove', (event) => {
+            if (this.dragging) {
+                this.move(event.clientX);
+            } else
+                return
+        });
+
+
+    }
+
+    adjustImagesSize = () => {
+        this.imagesContainerWidth = this.imagesContainerEl.offsetWidth;
+        this.imgEl1.style.width = `${this.imagesContainerWidth}px`;
+        this.imgEl2.style.width = this.imagesContainerWidth + 'px';
+        this.imagesContainerLeftOffset = this.imagesContainerEl.offsetLeft;
+        window.addEventListener('resize', this.adjustImagesSize);
+    }
+
+    init() {
+        this.events();
+        this.adjustImagesSize();
+
+    }
+
+}
---
+++ app/js/app_original.js
@@ -0,0 +1,88 @@
+import '../sass/index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+const imagesContainerEl = document.querySelector('.slider__images-container');
+const imgEl1 = document.querySelector('.slider__image-container--first img');
+const imgEl2 = document.querySelector('.slider__image-container--second img');
+let dragging = false;
+// odleglosc od lewej krawedzi
+// const img1ContainerEl =  document.querySelector('.slider__image-container--first');
+const img2ContainerEl = document.querySelector('.slider__image-container--second');
+const handleEl = document.querySelector('.slider__handle');
+const dividerEl = document.querySelector('.slider__divider');
+let imagesContainerWidth;
+let imagesContainerLeftOffset; 
+
+
+// clientX to przesuniecie w lini poziomej w px - potrzebna do slidera
+//clientX
+const getOffset = (clientX) => { 
+    const offset = clientX - imagesContainerLeftOffset;
+    if (offset < 0) {
+        return 0;
+    } else if (offset > imagesContainerWidth){
+        return imagesContainerWidth
+    }
+    else return offset;
+};
+
+const move = (clientX) => {
+    const offset = getOffset(clientX);
+    // zmierzamy do wartosci procentowej
+    const percent = offset/imagesContainerWidth * 100;
+    console.log(percent);
+    
+    dividerEl.style.left = `${percent}%`;
+    //zmieniamy drugiemu obrazkowi  wielkosc
+    img2ContainerEl.style.width = `${percent}%`;
+}
+
+
+// funkcja wywolujaca
+const initEvents= ()=> {
+    handleEl.addEventListener('mousedown', () => {dragging = true});
+    handleEl.addEventListener('mouseup', () => {dragging = false});
+    //urzadzenia mobilne 
+    handleEl.addEventListener('touchstart', () => {dragging = true});
+    handleEl.addEventListener('touchend', () => {dragging = false});
+
+    window.addEventListener('touchmove', (event) => {
+        if (dragging) {
+            move(event.touches[0].clientX);
+        } else
+         return
+    });
+
+    //przekazujemy event
+    window.addEventListener('mousemove', (event) => {
+        if (dragging) {
+            move(event.clientX);
+        } else
+         return
+    });
+
+
+
+}
+
+const adjustImagesSize = ()=> {
+    imagesContainerWidth = imagesContainerEl.offsetWidth;
+    imgEl1.style.width = `${imagesContainerWidth}px`;
+    imgEl2.style.width = imagesContainerWidth + 'px';
+    imagesContainerLeftOffset = imagesContainerEl.offsetLeft;
+    window.addEventListener('resize', adjustImagesSize);
+}
+
+adjustImagesSize();
+initEvents();
+
+
+
+
+
+
+
+
+});
